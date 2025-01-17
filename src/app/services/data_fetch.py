b7df1c07414f1e800a534fc95e53f02f8ec63aa2
Sandro Marcelo Peirano Gozalvez <sandromarcelo.peirano@gmail.com>
2025-01-17 14:48:14 -0500

    Load data from API into db

--- src/app/services/data_fetch.py
+++ src/app/services/data_fetch.py
@@ -33,10 +33,16 @@ class PokeAPIService:
             }
             return shapedData
 
-    async def fetch_location_area_urls(self, url):
+    async def fetch_location_area_urls_data(self, url):
         async with self.session.get(url) as resp:
             data = await resp.json()
-            shaped_data = [la["location_area"]["url"] for la in data]
+            shaped_data = [
+                {
+                    "url": la["location_area"]["url"],
+                    "name": la["location_area"]["name"],
+                }
+                for la in data
+            ]
             return shaped_data
 
     async def fetch_location_area(self, url):
@@ -46,6 +52,7 @@ class PokeAPIService:
                 "api_id": data["id"],
                 "name": data["name"],
                 "location_url": data["location"]["url"],
+                "location_name": data["location"]["name"]
             }
             return shaped_data
 
@@ -71,61 +78,3 @@ class PokeAPISessionManager:
     async def __aexit__(self, exc_type, exc_val, traceback):
         await self.poke_api_service.session.close()
 
-
-async def fetch_pokemon(pokemon):
-    async with aiohttp.ClientSession(BASE_URL) as session:
-        async with session.get(f"pokemon/{pokemon}") as resp:
-            data = await resp.json()
-
-            try:
-                type_two = data["types"][1]["type"]["name"]
-            except IndexError:
-                type_two = None
-
-            shapedData = {
-                "api_id": data["id"],
-                "name": data["name"],
-                "height": data["height"],
-                "weight": data["weight"],
-                "sprite": data["sprites"]["other"]["official-artwork"][
-                    "front_default"
-                ],
-                "type_one": data["types"][0]["type"]["name"],
-                "type_two": type_two,
-                "location_area_encounters": data.get(
-                    "location_area_encounters"
-                ),
-            }
-            return shapedData
-
-
-async def fetch_location_area_urls(url):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            data = await resp.json()
-            shaped_data = [la["location_area"]["url"] for la in data]
-            return shaped_data
-
-
-async def fetch_location_area(url):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            data = await resp.json()
-            shaped_data = {
-                "api_id": data["id"],
-                "name": data["name"],
-                "location_url": data["location"]["url"],
-            }
-            return shaped_data
-
-
-async def fetch_location(url):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            data = await resp.json()
-            shaped_data = {
-                "api_id": data["id"],
-                "name": data["name"],
-                "region": data["region"]["name"],
-            }
-            return shaped_data
--- src/app/services/data_loader.py
+++ src/app/services/data_loader.py
@@ -10,26 +10,85 @@ from .data_fetch import (
     PokeAPIService,
 )
 
+from .pokemon_query import PokemonQueryService
+
+from app import db
+
 
 class PokeAPIDataLoader:
     pokemon_to_load = ["ditto", "charizard"]
+    query_service = PokemonQueryService(db)
 
     async def load_pokemon_data(self):
         async with PokeAPISessionManager(PokeAPIService()) as svc:
-
             for pokemon in self.pokemon_to_load:
+                pokemon_in_db = self.query_service.query_pokemon_by_name(
+                    pokemon
+                )
+                if pokemon_in_db:
+                    return
+
                 pokemon_data = await svc.fetch_pokemon(pokemon)
                 location_area_encounters_url = pokemon_data[
                     "location_area_encounters"
                 ]
-                location_area_urls = await svc.fetch_location_area_urls(
-                    location_area_encounters_url
+                location_area_encounters_list = (
+                    await svc.fetch_location_area_urls_data(
+                        location_area_encounters_url
+                    )
                 )
-                print(pokemon_data, location_area_urls)
+                location_area_ids = []
+                if location_area_encounters_list:
+                    for data in location_area_encounters_list:
 
-                if location_area_urls:
-                    for url in location_area_urls:
-                        location_area_data = await svc.fetch_location_area(url)
-                        location_url = location_area_data["location_url"]
-                        location_data = await svc.fetch_location(location_url)
-                        print(location_area_data, location_data)
+                        location_area_in_db = (
+                            self.query_service.query_location_area_by_name(
+                                data["name"]
+                            )
+                        )
+
+                        if not location_area_in_db:
+                            location_area_data = await svc.fetch_location_area(
+                                data["url"]
+                            )
+
+                            location_url = location_area_data["location_url"]
+                            location_in_db = (
+                                self.query_service.query_location_by_name(
+                                    location_area_data["location_name"]
+                                )
+                            )
+
+                            if not location_in_db:
+                                location_data = await svc.fetch_location(
+                                    location_url
+                                )
+                                location_in_db = (
+                                    self.query_service.create_location(
+                                        location_data
+                                    )
+                                )
+
+                            location_area_data["location_id"] = (
+                                location_in_db.id
+                            )
+                            location_area_in_db = (
+                                self.query_service.create_location_area(
+                                    location_area_data
+                                )
+                            )
+                        location_area_ids.append(location_area_in_db.id)
+
+                    pokemon_in_db = self.query_service.create_pokemon(
+                        pokemon_data
+                    )
+                    self.query_service.insert_pokemon_id_location_area_ids(
+                        pokemon_in_db.id, location_area_ids
+                    )
+                    print(f"Pokemon {pokemon_in_db} loaded")
+
+                else:
+                    pokemon_in_db = self.query_service.create_pokemon(
+                        pokemon_data
+                    )
+                    print(f"Pokemon {pokemon_in_db} loaded")
